7a36d598533c178d1848d80e817507cd014a8aa9
loganafrica <65195397+loganafrica@users.noreply.github.com>
2024-02-27 10:29:18 +0200

    Create mediawiki.sh

---
+++ mediawiki.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt upgrade -y 
+sudo apt install nginx php php-fpm php-mysql php-xml php-curl php-dom
+sudo systemctl enable --now nginx
+sudo systemctl status nginx
+cd /tmp
+wget https://releases.wikimedia.org/mediawiki/1.41/mediawiki-1.41.0.tar.gz
+sudo mkdir /var/www/mediawiki
+tar xvfz mediawiki-1.41.0.tar.gz
+mv ./mediawiki-1.41.0/* /var/www/mediawiki/
+rm ./mediawiki-1.41.0.tar.gz -rf
